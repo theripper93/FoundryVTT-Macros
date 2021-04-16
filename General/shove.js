a44910fe8c8676f151e1b5cda82cde9ac130ac08
theripper93 <tsplab@gmail.com>
2021-04-16 19:31:44 +0200

    Add files via upload

---
+++ General/shove.js
@@ -0,0 +1,46 @@
+// Shove Macro add as "Shove" execute as GM
+// Knockback Macro add as "Knockback" execute as GM (knockback macro in the repository unde general)
+// Item : on use macro ItemMacro
+// Content of itemmacro -> await game.macros.getName("Shove").execute(canvas.tokens.get(args[0].tokenId),Array.from(game.user.targets)[0]);
+    let pusher = args[0];
+    let target = args[1]; 
+    let tokenRoll = await token.actor.rollSkill("ath");
+    let skill = target.actor.data.data.skills.ath.total < Target.actor.data.data.skills.acr.total ? "acr" : "ath";
+    let tactorRoll = await Target.actor.rollSkill(Skill);
+
+    if (tokenRoll.total > tactorRoll.total) {
+    if(await yesNoPrompt("title","content")){
+        await game.macros.getName("Knockback").execute(pusher,target,5);
+    }
+    else{
+        await game.cub.addCondition("Prone", Target.actor);
+    }
+    
+    }
+    
+    async function yesNoPrompt (dTitle,dContent){
+
+        let dialog = new Promise((resolve, reject) => {
+            new Dialog({
+            // localize this text
+            title: `${dTitle}`,
+            content: `<p>${dContent}</p>`,
+            buttons: {
+                one: {
+                    icon: '<i class="fas fa-check"></i>',
+                    label: `Knockback`,
+                    callback: () => {resolve(true)}
+                },
+                two: {
+                    icon: '<i class="fas fa-times"></i>',
+                    label: `Prone`,
+                    callback: () => {resolve(false)}
+                }
+            },
+            default: "two"
+            }).render(true);
+          });
+          let result = await dialog;
+          return result;
+    
+    }
