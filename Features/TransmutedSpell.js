e085b5624f09b8350c443402fc1324488857a067
theripper93 <tsplab@gmail.com>
2021-04-16 16:07:32 +0200

    Update TransmutedSpell.js

--- Features/TransmutedSpell.js
+++ Features/TransmutedSpell.js
@@ -1,5 +1,6 @@
 // Macro that lets you change the spell damage type when you cast a spell.
 // Add as macro and run once, it will keep running until the client refreshes
+// Do NOT run as GM
 
 const spFeatureName = "Scorcery Points";
 const damageTypeList = ["acid", "cold", "fire", "lightning", "poison", "thunder"];
