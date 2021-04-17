c7a7f403d08aa54a451649245a274b38dbf467e5
theripper93 <tsplab@gmail.com>
2021-04-17 18:46:24 +0200

    Update lightSwitchGmMacro.js

--- General/lightSwitchGmMacro.js
+++ General/lightSwitchGmMacro.js
@@ -2,6 +2,10 @@
 // Macro 1: Name : lightSwitchPlayer Content: game.macros.getName("lightSwitchGmMacro").execute(token.id);
 // Macro 2: Name : lightSwitchGmMacro Content: Below
 let triggerToken = canvas.tokens.get(args[0]);
+if(!triggerToken){
+    ui.notifications.info(`No token selected!`);
+    return;
+} 
 const reducer = (previousLight, currentLight) => {return (canvas.grid.measureDistance(currentLight.center, triggerToken.center) < canvas.grid.measureDistance(previousLight.center, triggerToken.center)) && !triggerToken.checkCollision(currentLight.center) ? currentLight : previousLight;};
 let lightToSwitch = await canvas.lighting.placeables.reduce(reducer, canvas.lighting.placeables[0]);
 if(canvas.grid.measureDistance(triggerToken.center, lightToSwitch.center) < 10) lightToSwitch.update({"hidden": !lightToSwitch.data.hidden});
@@ -9,6 +13,6 @@ if(canvas.grid.measureDistance(triggerToken.center, lightToSwitch.center) < 10) 
 // Create a token, call it lightswitchtoken (example) give it a transparent image and place it as hidden
 // On your "Trigger Happy" journal entry add the following line
 //  @Token[lightswitchtoken]@Trigger[click]
-// the drag the lightSwitchPlayer macro from your hotbar to the journal. it should end up loking something like this
+// the drag the lightSwitchPlayer macro from your hotbar to the journal. it should end up looking something like this
 // but with the correct Id
 // @Token[lightSwitch]@Trigger[click]@Macro[1ENFpqAKMFKgnc5q]{lightSwitchPlayer}
