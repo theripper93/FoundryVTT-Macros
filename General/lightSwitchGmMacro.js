89dc56f7189454f6c85236a88d1f632df5b53d20
theripper93 <tsplab@gmail.com>
2021-04-17 18:44:10 +0200

    Update lightSwitchGmMacro.js

--- General/lightSwitchGmMacro.js
+++ General/lightSwitchGmMacro.js
@@ -2,9 +2,9 @@
 // Macro 1: Name : lightSwitchPlayer Content: game.macros.getName("lightSwitchGmMacro").execute(token.id);
 // Macro 2: Name : lightSwitchGmMacro Content: Below
 let triggerToken = canvas.tokens.get(args[0]);
-const reducer = (previousLight, currentLight) => {return canvas.grid.measureDistance(currentLight.center, triggerToken.center) < canvas.grid.measureDistance(previousLight.center, triggerToken.center) ? currentLight : previousLight;};
+const reducer = (previousLight, currentLight) => {return (canvas.grid.measureDistance(currentLight.center, triggerToken.center) < canvas.grid.measureDistance(previousLight.center, triggerToken.center)) && !triggerToken.checkCollision(currentLight.center) ? currentLight : previousLight;};
 let lightToSwitch = await canvas.lighting.placeables.reduce(reducer, canvas.lighting.placeables[0]);
-lightToSwitch.update({"hidden": !lightToSwitch.data.hidden});
+if(canvas.grid.measureDistance(triggerToken.center, lightToSwitch.center) < 10) lightToSwitch.update({"hidden": !lightToSwitch.data.hidden});
 // Trigger Happy Setup:
 // Create a token, call it lightswitchtoken (example) give it a transparent image and place it as hidden
 // On your "Trigger Happy" journal entry add the following line
